76e229910eaa3839ab1cca208355247fec423a0b
hayasehiise <kuronegahiise@gmail.com>
2025-06-26 00:55:41 +0800

    Ini Versi React karena yang Vue kekurangan library

---
+++ src/components/navbar.tsx
@@ -0,0 +1,146 @@
+import { Box, Button, Flex, Image, Link, Menu, Portal } from "@chakra-ui/react";
+import { FaAngleDoubleDown } from "react-icons/fa";
+
+export default function Navbar() {
+  return (
+    <Flex
+      direction={"row"}
+      padding={"2"}
+      justify={"space-between"}
+      backgroundColor={"blue.500"}
+    >
+      <Image
+        src={"image/brand.png"}
+        height={"20%"}
+        width={"20%"}
+        marginY={"auto"}
+      />
+      {/* bagian menu */}
+      <Flex direction={"row"} gap={"3"}>
+        <Button variant={"ghost"} border={"none"} asChild>
+          <Link
+            textDecoration={"none"}
+            color={"white"}
+            _hover={{ color: "black" }}
+            href="#"
+          >
+            Berita
+          </Link>
+        </Button>
+        <Menu.Root>
+          <Menu.Trigger asChild>
+            <Button
+              variant={"ghost"}
+              border={"none"}
+              color={"white"}
+              _hover={{ color: "black" }}
+              _open={{ color: "black" }}
+            >
+              Portal Informasi <FaAngleDoubleDown />
+            </Button>
+          </Menu.Trigger>
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                <Menu.Item
+                  value="informasi-umum-bandara"
+                  cursor={"pointer"}
+                  asChild
+                >
+                  <Link>Informasi Umum Bandara</Link>
+                </Menu.Item>
+                <Menu.Item value="sejarah" asChild cursor={"pointer"}>
+                  <Link>Sejarah</Link>
+                </Menu.Item>
+                <Menu.Item value="tugas-dan-fungsi" asChild cursor={"pointer"}>
+                  <Link>Tugas & Fungsi</Link>
+                </Menu.Item>
+                <Menu.Item
+                  value="struktur-organisasi"
+                  asChild
+                  cursor={"pointer"}
+                >
+                  <Link>Struktur Organisasi</Link>
+                </Menu.Item>
+                <Menu.Item value="visi-dan-misi" asChild cursor={"pointer"}>
+                  <Link>Visi & Misi</Link>
+                </Menu.Item>
+                <Menu.Item
+                  value="maklumat-pelayanan"
+                  asChild
+                  cursor={"pointer"}
+                >
+                  <Link>Maklumat Pelayanan</Link>
+                </Menu.Item>
+                <Menu.Item
+                  value="tarif-badan-layanan-umum"
+                  asChild
+                  cursor={"pointer"}
+                >
+                  <Link>Tarif Badan Layanan Umum</Link>
+                </Menu.Item>
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
+        <Menu.Root>
+          <Menu.Trigger asChild>
+            <Button
+              variant={"ghost"}
+              border={"none"}
+              color={"white"}
+              _hover={{ color: "black" }}
+              _open={{ color: "black" }}
+            >
+              Portal Sistem <FaAngleDoubleDown />
+            </Button>
+          </Menu.Trigger>
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                <Menu.Item value="simadu" asChild cursor={"pointer"}>
+                  <Link>Sistem Manajemen Pengaduan</Link>
+                </Menu.Item>
+                <Menu.Item value="permohonan-pas" asChild cursor={"pointer"}>
+                  <Link>Permohonan PAS Bandara</Link>
+                </Menu.Item>
+                <Menu.Item value="informasi-ppid" asChild cursor={"pointer"}>
+                  <Link>Permohonan Informasi PPID</Link>
+                </Menu.Item>
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
+        <Menu.Root>
+          <Menu.Trigger asChild>
+            <Button
+              variant={"ghost"}
+              border={"none"}
+              color={"white"}
+              _hover={{ color: "black" }}
+              _open={{ color: "black" }}
+            >
+              Portal Support <FaAngleDoubleDown />
+            </Button>
+          </Menu.Trigger>
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                <Menu.Item value="help" asChild cursor={"pointer"}>
+                  <Link>Help & Support</Link>
+                </Menu.Item>
+                <Menu.Item value="kontak" asChild cursor={"pointer"}>
+                  <Link>Kontak</Link>
+                </Menu.Item>
+                <Menu.Item value="gratifikasi" asChild cursor={"pointer"}>
+                  <Link>Pengaduan Gratifikasi</Link>
+                </Menu.Item>
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
+      </Flex>
+      {/* end menu */}
+    </Flex>
+  );
+}
