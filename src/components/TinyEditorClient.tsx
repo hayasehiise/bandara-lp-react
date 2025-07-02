c157e90c52cba5ee435bb6a158d975d93bb6cf44
hayasehiise <kuronegahiise@gmail.com>
2025-07-02 20:50:29 +0800

    Ada Perubahan jadi Self Hosted

--- src/components/TinyEditorClient.tsx
+++ src/components/TinyEditorClient.tsx
@@ -1,5 +1,54 @@
 "use client";
 import { Editor } from "@tinymce/tinymce-react";
+// TinyMCE so the global var exists
+import "tinymce/tinymce";
+// DOM model
+import "tinymce/models/dom/model";
+// Theme
+import "tinymce/themes/silver";
+// Toolbar icons
+import "tinymce/icons/default";
+// Editor styles
+import "tinymce/skins/ui/oxide/skin";
+
+// importing the plugin js.
+// if you use a plugin that is not listed here the editor will fail to load
+import "tinymce/plugins/advlist";
+import "tinymce/plugins/anchor";
+import "tinymce/plugins/autolink";
+import "tinymce/plugins/autoresize";
+import "tinymce/plugins/autosave";
+import "tinymce/plugins/charmap";
+import "tinymce/plugins/code";
+import "tinymce/plugins/codesample";
+import "tinymce/plugins/directionality";
+import "tinymce/plugins/emoticons";
+import "tinymce/plugins/fullscreen";
+import "tinymce/plugins/help";
+import "tinymce/plugins/help/js/i18n/keynav/en";
+import "tinymce/plugins/image";
+import "tinymce/plugins/importcss";
+import "tinymce/plugins/insertdatetime";
+import "tinymce/plugins/link";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/media";
+import "tinymce/plugins/nonbreaking";
+import "tinymce/plugins/pagebreak";
+import "tinymce/plugins/preview";
+import "tinymce/plugins/quickbars";
+import "tinymce/plugins/save";
+import "tinymce/plugins/searchreplace";
+import "tinymce/plugins/table";
+import "tinymce/plugins/visualblocks";
+import "tinymce/plugins/visualchars";
+import "tinymce/plugins/wordcount";
+
+// importing plugin resources
+import "tinymce/plugins/emoticons/js/emojis";
+
+// Content styles, including inline UI like fake cursors
+import "tinymce/skins/content/default/content";
+import "tinymce/skins/ui/oxide/content";
 
 export default function TinyEditorClient({
   value,
@@ -10,7 +59,7 @@ export default function TinyEditorClient({
 }) {
   return (
     <Editor
-      apiKey="b91fiejabhuvspq4vrw3j0chvds7dfax72i01ewvbdis6zda"
+      apiKey="no-api-key"
       value={value}
       onEditorChange={onChange}
       init={{
@@ -46,7 +95,6 @@ export default function TinyEditorClient({
           "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         automatic_uploads: true,
         images_reuse_filename: false,
-        // @ts-ignore: TinyMCE v7 expects 4 args, types still assume v6
         images_upload_url: "/api/upload",
       }}
     />
