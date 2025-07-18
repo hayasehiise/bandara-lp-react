e2249612fab773a1c9206ae19a8f0db6d201843b
hayasehiise <kuronegahiise@gmail.com>
2025-07-18 17:04:49 +0800

    aman sdh

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -1,12 +1,5 @@
 "use client";
-import {
-  Accordion,
-  Button,
-  Flex,
-  Span,
-  Link as ChakraLink,
-  VStack,
-} from "@chakra-ui/react";
+import { Button, Flex, Link as ChakraLink, VStack } from "@chakra-ui/react";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 
@@ -25,40 +18,32 @@ export default function SidebarDashboard() {
         paddingBottom={10}
         justify={"space-between"}
       >
-        <VStack alignItems={"start"}>
-          <Accordion.Root variant={"subtle"} collapsible>
-            <Accordion.Item value="news">
-              <Accordion.ItemTrigger
-                backgroundColor={"gray.200"}
-                color={"black"}
-                borderRadius={0}
-              >
-                <Span>Berita</Span>
-                <Accordion.ItemIndicator />
-              </Accordion.ItemTrigger>
-              <Accordion.ItemContent
-                backgroundColor={"gray.200"}
-                color={"white"}
-                borderRadius={0}
-              >
-                <Accordion.ItemBody>
-                  <VStack alignItems={"start"} paddingLeft={4}>
-                    <ChakraLink variant={"plain"} asChild>
-                      <Link href={"/dashboard/news"}>List Berita</Link>
-                    </ChakraLink>
-                    <ChakraLink variant={"plain"} asChild>
-                      <Link href={"/dashboard/news/create"}>Tambah Berita</Link>
-                    </ChakraLink>
-                  </VStack>
-                </Accordion.ItemBody>
-              </Accordion.ItemContent>
-            </Accordion.Item>
-          </Accordion.Root>
+        <VStack alignItems={"start"} width={"full"}>
           <ChakraLink
             variant={"plain"}
             paddingLeft={4}
-            _hover={{ textDecor: "none" }}
+            paddingY={3}
+            _hover={{ textDecor: "none", backgroundColor: "white" }}
+            transition={"background 0.5s ease-in-out"}
+            _focus={{
+              outline: "none",
+              boxShadow: "none",
+            }}
+            fontWeight={"semibold"}
+            width={"full"}
+            asChild
+          >
+            <Link href={"/dashboard/news"}>Berita</Link>
+          </ChakraLink>
+          <ChakraLink
+            variant={"plain"}
+            paddingLeft={4}
+            paddingY={3}
+            _hover={{ textDecor: "none", backgroundColor: "white" }}
+            transition={"background 0.5s ease-in-out"}
             _focus={{ outline: "none", boxShadow: "none" }}
+            fontWeight={"semibold"}
+            width={"full"}
             asChild
           >
             <Link href={"/dashboard/category"}>Kategori</Link>
